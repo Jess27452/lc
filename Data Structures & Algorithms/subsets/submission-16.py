9a7b69509857af41c20153d6ec266d50fbabf865
Jess27452 <jessicazzz1122@gmail.com>
2026-09-25 17:04:40 -0400

    Add: subsets - submission-16

---
+++ Data Structures & Algorithms/subsets/submission-16.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[[]]
+        subset=[]
+        def dfs(start):
+            for i in range(start,len(nums)):
+                subset.append(nums[i])
+                res.append(subset.copy())
+                dfs(i+1)
+                subset.pop()
+        dfs(0)
+        return res
